c0d9a6d92a0a2377826c9e1c44c6c47945055d06
juanfezaga <juanfezaga@gmail.com>
2021-11-10 17:43:04 -0500

    Bug fix in useMarkeplace and managment cleanup

--- src/pages/Management/Management.tsx
+++ src/pages/Management/Management.tsx
@@ -4,17 +4,12 @@ import {
   IonContent,
   IonFab,
   IonFabButton,
-  IonCard,
-  IonCardHeader,
-  IonCardContent,
-  IonImg,
 } from "@ionic/react";
-import React, { useEffect } from "react";
+import React from "react";
 import { add } from "ionicons/icons";
 
 import "./Management.css";
 import Header from "../../components/Header/Header";
-import usePosts from "../../hooks/usePosts";
 
 type Props = {
   history: {
@@ -23,38 +18,24 @@ type Props = {
 };
 
 const ManagementPage: React.FC<Props> = ({ history }) => {
-  const { projectsPosts, fetchProjects } = usePosts();
-  useEffect(() => {
-    fetchProjects();
-  }, [])
   return (
     <IonPage>
       <Header canBack={false} title="Gestion de obra" />
       <IonContent>
-        {projectsPosts ? (
-          <div className="container-management">
-            {projectsPosts.map((project: any) => (
-              <IonCard key={project._id}>
-                <IonCardHeader>{project.title}</IonCardHeader>
-                <IonCardContent>
-                    <IonImg src={project.imagesUrl[0]} />
-                </IonCardContent>
-              </IonCard>
-            ))}
-          </div>
-        ) : (
-          <div className="container-management">
+        <div className="container-management">
           <span className="empty_project_message">
             No tienes aun ningun proyecto creado
           </span>
         </div>
-        )}
-        
+
         <IonFab vertical="bottom" horizontal="end" slot="fixed">
           <IonFabButton>
-            <IonIcon  onClick={() => {
-              history.push('/create-project')
-            }} icon={add} />
+            <IonIcon
+              onClick={() => {
+                history.push("/create-project");
+              }}
+              icon={add}
+            />
           </IonFabButton>
         </IonFab>
       </IonContent>
